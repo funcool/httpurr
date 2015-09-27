b02d3ed2189f39049c0a6c6971a23f92e8f36c2f
Alejandro Gómez <alejandro@dialelo.com>
2015-09-27 11:25:27 +0200

    Bump first release

--- project.clj
+++ project.clj
@@ -1,12 +1,17 @@
-(defproject funcool/httpurr "0.1.0-SNAPSHOT"
+(defproject funcool/httpurr "0.1.0"
   :description ""
   :url "http://funcool.github.io/httpurr"
-  :license {:name ""
-            :url ""}
+  :license {:name "Public Domain"
+            :url "http://unlicense.org"}
 
   :dependencies [[org.clojure/clojurescript "1.7.122" :scope "provided"]
                  [funcool/cats "1.0.0"]
-                 [funcool/promesa "0.5.0"]
-                 [org.clojure/test.check "0.8.2"]]
+                 [funcool/promesa "0.5.1"]]
+
+  :profiles {
+    :dev {
+      :dependencies [[org.clojure/test.check "0.8.2"]]
+    }
+  }
 
   :source-paths ["src"])
