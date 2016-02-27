d1554a78adebc3495a30d17bae76e20b4c0e314d
Andrey Antukh <niwi@niwi.nz>
2016-02-27 10:37:11 +0200

    Update dependencies and remove unused dependencies.

--- project.clj
+++ project.clj
@@ -2,12 +2,12 @@
   :description "A ring-inspired, promise-returning, simple ClojureScript HTTP client."
   :url "http://funcool.github.io/httpurr"
   :license {:name "Public Domain" :url "http://unlicense.org"}
-
-  :dependencies [[org.clojure/clojure "1.7.0" :scope "provided"]
-                 [org.clojure/clojurescript "1.7.189" :scope "provided"]
+  :source-paths ["src"]
+  :dependencies [[org.clojure/clojure "1.8.0" :scope "provided"]
+                 [org.clojure/clojurescript "1.7.228" :scope "provided"]
                  [org.clojure/test.check "0.9.0" :scope "test"]
-                 [funcool/cats "1.2.1"]
-                 [funcool/promesa "0.7.0"]]
+                 [funcool/promesa "0.8.1"]]
 
-  :plugins [[lein-ancient "0.6.7"]]
-  :source-paths ["src"])
+  :profiles
+  {:dev
+   {:plugins [[lein-ancient "0.6.7"]]}})
