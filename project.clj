e8a866c673472d29100cc41675792e4465f49e11
Andrey Antukh <niwi@niwi.nz>
2016-07-10 13:40:36 +0300

    Update project.clj

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/httpurr "0.6.0"
+(defproject funcool/httpurr "0.6.1"
   :description "A ring-inspired, promise-returning, simple Clojure(Script) HTTP client."
   :url "http://funcool.github.io/httpurr"
   :license {:name "Public Domain" :url "http://unlicense.org"}
@@ -7,8 +7,8 @@
                  [org.clojure/clojurescript "1.8.40" :scope "provided"]
                  [aleph "0.4.1" :scope "provided"]
                  [org.clojure/test.check "0.9.0" :scope "test"]
-                 [funcool/promesa "1.1.1"]]
+                 [funcool/promesa "1.4.0"]]
 
   :profiles
   {:dev
-   {:plugins [[lein-ancient "0.6.7"]]}})
+   {:plugins [[lein-ancient "0.6.10"]]}})
