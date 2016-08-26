36f610a0ac5e4c75cbc09f2015aa6d2526516c22
Andrey Antukh <niwi@niwi.nz>
2016-08-26 08:21:46 +0300

    Set version to 0.6.2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject funcool/httpurr "0.6.1"
+(defproject funcool/httpurr "0.6.2"
   :description "A ring-inspired, promise-returning, simple Clojure(Script) HTTP client."
   :url "http://funcool.github.io/httpurr"
   :license {:name "Public Domain" :url "http://unlicense.org"}
