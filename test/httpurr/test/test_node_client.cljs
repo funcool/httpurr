7e9a007345e6abe43f8726a6ea57fd93b8d9a254
Lars Trieloff <lars@trieloff.net>
2017-05-21 17:06:54 +0200

    Test with httpurr’s own binaries

--- test/httpurr/test/test_node_client.cljs
+++ test/httpurr/test/test_node_client.cljs
@@ -101,18 +101,31 @@
                   (t/is (= (:path lreq) path))
                   (done)))))))
 
-(t/deftest send-binary
+(t/deftest send-small-binary
   (t/async done
-    (let [path "/binary"
-          uri (make-uri path)
+    (let [uri "https://clojars.org/repo/funcool/httpurr/0.6.2/httpurr-0.6.2.jar.md5"
           req {:method :get
                :url uri
                :headers {}}]
 
       (p/then (send! req)
               (fn [response]
-                (println (:body response))
-                (done))))))
+                (t/is (= (get (:headers response) "Content-Length") (str (count (:body response)))))
+                (done)
+                )))))
+
+(t/deftest send-medium-binary
+  (t/async done
+    (let [uri "https://clojars.org/repo/funcool/httpurr/0.6.2/httpurr-0.6.2.jar"
+          req {:method :get
+               :url uri
+               :headers {}}]
+
+      (p/then (send! req)
+              (fn [response]
+                (t/is (= (get (:headers response) "Content-Length") (str (count (:body response)))))
+                (done)
+                )))))
 
 
 (t/deftest send-plain-get-with-query-string
